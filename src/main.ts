d9f7c7fe819bae16bd230df4cea973789c81bce2
vpuska-laptop <vpuska@outlook.com>
2024-11-30 13:58:06 +1100

    ENABLE CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import {ValidationPipe} from "@nestjs/common";  // <-- insert statement
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: true,
+        methods: ['GET']
+    });
     app.useGlobalPipes(new ValidationPipe({     // <-- insert statement
         transform: true,
         whitelist: true,
